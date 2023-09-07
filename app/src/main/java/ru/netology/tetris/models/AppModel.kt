027fb2ae94195d3f4595455bcc223900a69e8000
Storozhuk Denis <Storozhuk.Denis@gmail.com>
2023-09-07 13:51:34 +0300

    creating blocks function

--- app/src/main/java/ru/netology/tetris/models/AppModel.kt
+++ app/src/main/java/ru/netology/tetris/models/AppModel.kt
@@ -17,16 +17,45 @@ class AppModel {
         FieldConstants.FieldConstatns.ROW_COUNT.value,
         FieldConstants.FieldConstatns.COLUMN_COUNT.value
     )
-    
-
-
-
-
     enum class Statuses{
         AWAITING_START, ACTIVE, INACTIVE, OVER
     }
     enum class Motions {
         LEFT, RIGHT, DOWN, ROTATE
+    }
+    fun setPreferences(preferences: AppPreferences?){
+        this.preferences = preferences
+    }
+
+    fun getCellStatus(row: Int, column: Int): Byte?{
+        return field[row] [column]
+    }
+
+    private fun setCellStatus(row:Int,column: Int,status: Byte?){
+        if (status!= null){
+            field[row][column] = status
+        }
+    }
+
+    fun isgameOver(): Boolean{
+        return currentState == Statuses.OVER.name
+    }
+
+    fun isGameActive(): Boolean{
+        return  currentState == Statuses.ACTIVE.name
+    }
+
+    fun isGameAwaitingStart():Boolean{
+        return currentState == Statuses.AWAITING_START.name
+    }
+
+    private fun bootScore(){
+        score += 10
+        if (score> preferences?.getHigtScore() as Int)
+            preferences?.saveHighScore(score)
+    }
+    private fun generateNextBlock(){
+        currentBlock = Block.createBlock()
     }
 
 
